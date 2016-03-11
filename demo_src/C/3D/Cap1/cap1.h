a0be41e3224ec40fb94307b41eaee57ddba71584
Simone Bna <s.bna@scsitaly.com>
2016-03-11 16:33:21 +0100

    added license header in the demo src files

--- demo_src/C/3D/Cap1/cap1.h
+++ demo_src/C/3D/Cap1/cap1.h
@@ -1,3 +1,21 @@
+/****************************************************************************
+ * Copyright (C) 2015 by Simone Bnà(a), Sandro Manservisi(a),               *
+ * Ruben Scardovelli(a), Philip Yecko(b) and Stephane Zaleski(c,d)          *
+ * (a) DIN–Lab. di Montecuccolino, Università di Bologna,                   *
+ *     Via dei Colli 16, 40136 Bologna, Italy                               *
+ * (b) Physics Department, Cooper Union, New York, NY, USA                  *
+ * (c) Sorbonne Universités, UPMC Univ Paris 06, UMR 7190,                  *
+ *     Institut Jean Le Rond d’Alembert, F-75005, Paris, France             *
+ * (d) CNRS, UMR 7190, Institut Jean Le Rond d’Alembert, F-75005,           *
+ *     Paris, France                                                        *
+ *                                                                          *
+ * You should have received a copy of the CPC license along with Vofi.      *
+ * If not, see http://cpc.cs.qub.ac.uk/licence/licence.html.                *
+ *                                                                          *
+ * e-mail: ruben.scardovelli@unibo.it                                       *
+ *                                                                          *
+ ****************************************************************************/
+
 /* grid resolution */
 #define NMX   1
 #define NMY   1
